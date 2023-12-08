cbad39c0d891823445dc35aa7899173fa3244f86
andelkarim <148983827+andelkarim@users.noreply.github.com>
2023-12-08 12:08:07 +0100

    Create Echauffements.py

---
+++ Echauffements.py
@@ -0,0 +1,12 @@
+def produce_default_dict():
+    default_dict = {'root': 'password'}
+    return default_dict
+
+# Exemple d'appel à la fonction (non déclenché lors de l'import dans un autre script)
+if __name__ == "__main__":
+    my_default_dict = produce_default_dict()
+    print(my_default_dict)  # Vous pouvez ajouter d'autres opérations avec le dictionnaire ici
+
+
+
+
