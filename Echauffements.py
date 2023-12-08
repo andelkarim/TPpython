3f9dc43c81b9418eded428d235f1cd5b8eb12ea6
andelkarim <148983827+andelkarim@users.noreply.github.com>
2023-12-08 12:17:50 +0100

    Update Echauffements.py

--- Echauffements.py
+++ Echauffements.py
@@ -2,10 +2,29 @@ def produce_default_dict():
     default_dict = {'root': 'password'}
     return default_dict
 
-# Exemple d'appel à la fonction (non déclenché lors de l'import dans un autre script)
+# Exemple d'appel à la fonction
 if __name__ == "__main__":
     my_default_dict = produce_default_dict()
-    print(my_default_dict)  # Vous pouvez ajouter d'autres opérations avec le dictionnaire ici
+    print(my_default_dict)  #autres opérations avec le dictionnaire ici
+
+
+def salutation(nom, age):
+    if age == '1':
+        age_txt = "1 an"
+    else:
+        age_txt = f"{age} ans"
+
+    message = f"Bonjour '{nom}', vous avez actuellement {age_txt}."
+    return message
+
+# Exemple d'appel à la fonction
+resultat_1 = salutation('gael', '24')
+print(resultat_1)  # Affiche le message pour 'gael' et 24 ans
+
+resultat_2 = salutation('bébé', '0')
+print(resultat_2)  # Affiche le message pour 'bébé' et 0 an
+
+
 
 
 
